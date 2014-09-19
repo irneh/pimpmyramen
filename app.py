3f10315aacaf1840961e031a26239e145b682fd2
Henri Gabriel <henri@workforpizza.com>
2014-09-18 19:55:11 -0700

    Write to S3.

--- app.py
+++ app.py
@@ -25,9 +25,23 @@ c = boto.connect_s3()
 b = c.get_bucket(S3_BUCKET)
 k = boto.s3.key.Key(b)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return f.render_template('index.html')
+  if f.request.method == 'GET':
+    return f.render_template('index.html')
+  else:
+    ## Process incoming args
+    img = f.request.files['image']
+    ## Save incoming file to Flask-Uploads collection and local disk.
+    localname = str(uuid.uuid4()) + os.path.splitext(img.filename)[1]
+    images.save(img, None, localname)
+    ## Move file to S3
+    k.key = os.path.basename(localname)
+    k.set_contents_from_filename(images.path(localname))
+    os.remove(images.path(localname))
+    ## Return URL to S3 zipfile
+    url = 'http://' + S3_BUCKET + '/' + os.path.basename(localname)
+    return f.render_template('download.html', url=url)
 
 @app.route('/api', methods=['GET', 'POST'])
 def api():
