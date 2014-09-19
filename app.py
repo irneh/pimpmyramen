2696e2a4b3b21cc426df4209c0cac1aa69891a95
Henri Gabriel <henri@workforpizza.com>
2014-09-18 19:04:13 -0700

    Add AWS/S3 and Flask-Uploads.

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
+import boto
 import flask as f
+import flask.ext.uploads as fu
 import os
 import redis
 import time
 import urlparse
-
-url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
-r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+import uuid
 
 app = f.Flask(__name__)
 app.debug = os.getenv('APP_DEBUG')
+
+## Redis.
+url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
+
+## Flask-Uploads config.
+app.config['UPLOADED_IMAGES_DEST'] = 'tmp'
+images = fu.UploadSet('images', ('gif', 'bmp', 'png', 'jpg', 'jpeg'))
+fu.configure_uploads(app, (images))
+
+## S3.
+S3_BUCKET = os.getenv('S3_BUCKET')
+c = boto.connect_s3()
+b = c.get_bucket(S3_BUCKET)
+k = boto.s3.key.Key(b)
 
 @app.route('/', methods=['GET'])
 def index():
